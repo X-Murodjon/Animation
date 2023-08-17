0ba908001f0074488175482212d4686bf6e55057
X-Murodjon <xolboboyevmurodjon1997@gmail.com>
2023-08-17 18:02:22 +0500

    .

---
+++ Foods_GYM2/js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function(){
+    const tabs = document.querySelectorAll('.tabheader__item'),
+
+    tabsParent = document.querySelector('.tabheader__items')
+})
